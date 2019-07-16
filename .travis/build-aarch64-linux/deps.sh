7dac95739737b91bf28fc5e8ca6e1ac497b9614a
SachinVin <sachinvinayak2000@gmail.com>
2019-07-16 21:38:17 +0530

    travis a64: unicorn

--- .travis/build-aarch64-linux/deps.sh
+++ .travis/build-aarch64-linux/deps.sh
@@ -4,9 +4,13 @@ set -e
 set -x
 
 apt-get update
-apt-get install -y git cmake gcc ninja-build g++-8-aarch64-linux-gnu qemu-user
+apt-get install -y git cmake gcc python ninja-build g++-8-aarch64-linux-gnu qemu-user
 
 # TODO: This isn't ideal.
 cd dynarmic/externals
 git clone https://github.com/MerryMage/ext-boost
-cd ..
+git clone https://github.com/unicorn-engine/unicorn.git
+
+cd unicorn
+UNICORN_ARCHS="arm aarch64" CC=aarch64-linux-gnu-gcc-8 ./make.sh
+cd ../..
