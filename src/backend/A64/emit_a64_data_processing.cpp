157fce5ab916a9214132757ab00fe5b59a0cc9fc
SachinVin <sachinvinayak2000@gmail.com>
2019-07-16 17:34:37 +0530

    backend\A64\emit_a64_data_processing.cpp: Mostly empty file

---
+++ src/backend/A64/emit_a64_data_processing.cpp
@@ -0,0 +1,16 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2016 MerryMage
+ * This software may be used and distributed according to the terms of the GNU
+ * General Public License version 2 or any later version.
+ */
+
+#include "backend/A64/block_of_code.h"
+#include "backend/A64/emit_a64.h"
+#include "common/assert.h"
+#include "common/common_types.h"
+#include "frontend/ir/basic_block.h"
+#include "frontend/ir/microinstruction.h"
+#include "frontend/ir/opcodes.h"
+
+namespace Dynarmic::BackendA64 {
+} // namespace Dynarmic::BackendA64
