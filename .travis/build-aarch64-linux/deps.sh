47e508dd12a75fb0a34941c13479a1a44d9f28b9
SachinVin <sachinvinayak2000@gmail.com>
2020-05-16 23:33:24 +0530

    travis : a64: remove docker; dont fuzz against unicorn

--- .travis/build-aarch64-linux/deps.sh
+++ .travis/build-aarch64-linux/deps.sh
@@ -3,14 +3,7 @@
 set -e
 set -x
 
-apt-get update
-apt-get install -y git cmake gcc python ninja-build g++-8-aarch64-linux-gnu qemu-user
-
 # TODO: This isn't ideal.
-cd dynarmic/externals
+cd externals
 git clone https://github.com/MerryMage/ext-boost
-git clone https://github.com/unicorn-engine/unicorn.git
-
-cd unicorn
-UNICORN_ARCHS="arm aarch64" CC=aarch64-linux-gnu-gcc-8 ./make.sh
-cd ../..
+cd ..
